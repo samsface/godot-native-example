2aac50f88a39e3dd8eb01117545b35a5beaac304
Sam Murray <samsface@gmail.com>
2021-10-24 14:15:24 +0100

    Update ci.bash

--- ci/ci.bash
+++ ci/ci.bash
@@ -4,4 +4,16 @@ mkdir -p work
 cd work
 
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install/lib
-cmake --build . --config Release --target install/strip
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    CYGWIN*)    machine=windows;;
+    MINGW*)     machine=windows;;
+    *)          machine="${unameOut}"
+esac
+
+if [ "${machine}" = "windows" ]; then
+  cmake --build . --config Release --target install
+else
+  cmake --build . --config Release --target install/strip
+fi
